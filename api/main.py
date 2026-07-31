e3fa5981dc9a127879129ff6f4db2e7d35482262
Yashvir Bhardwaj <yashvirbhardwaj10@gmail.com>
2026-07-31 19:47:20 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,4 @@
 from typing import List
-
 import torch
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -23,7 +22,7 @@ app = FastAPI(title="IMDB Sentiment API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
